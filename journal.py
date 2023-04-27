be9ed6f6d644d19757f0943de9ef6bcd9315f22d
Sovela <96300775+MastersShifu@users.noreply.github.com>
2023-04-27 15:09:18 +0300

    Update journal.py

--- journal.py
+++ journal.py
@@ -7,14 +7,41 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=invalid-name
 
-from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit, QVBoxLayout, QWidget, QLabel, QStackedWidget
+from PyQt5.QtCore import Qt
 from login import LoginPage
+from register import RegisterPage
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.stacked_widget = QStackedWidget(self)
+        self.login_widget = LoginPage()
+        self.register_widget = RegisterPage()
+        self.stacked_widget.addWidget(self.login_widget)
+        self.stacked_widget.addWidget(self.register_widget)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.stacked_widget)
+        self.setLayout(layout)
+
+        self.login_widget.regi.clicked.connect(self.show_register)
+        self.register_widget.back_button.clicked.connect(self.show_login)
+        self.login_widget.login.clicked.connect(self.aut)
+
+    def show_login(self):
+        self.stacked_widget.setCurrentIndex(0)
+
+    def show_register(self):
+        self.stacked_widget.setCurrentIndex(1)
+    
+    def aut(self):
+        self.login_widget.autorizate()
 
 if __name__ == "__main__":
     app = QApplication([])
-    login_page = LoginPage()
-    login_page.setWindowState(Qt.WindowMaximized) 
-    login_page.show()
+    main_window = MainWindow()
+    main_window.setWindowState(Qt.WindowMaximized)
+    main_window.show()
     app.exec()
-        
